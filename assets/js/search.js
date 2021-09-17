3923b2e2a61ea2e3b2ee3d6b2d10be7d634a7eff
flizzer <flizzer@users.noreply.github.com>
2021-09-17 03:01:00 +0000

    deploy: 26c12d35d2838fa58f5d75631d25a62b508029f0

--- assets/js/search.js
+++ assets/js/search.js
@@ -47,7 +47,7 @@ jtd.onReady = function(ready) {
 
 function initSearch() {
     var request = new XMLHttpRequest();
-    request.open('GET', '/hacksbynight.org/assets/js/search-data.json', true);
+    request.open('GET', '/datascibri.com/assets/js/search-data.json', true);
   
     request.onload = function(){
       if (request.status >= 200 && request.status < 400) {
